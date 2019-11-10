d2e8ee4425be6b0542d158c7ad865f7a953b1d15
christopherschoening <schoeningchristopher@gmail.com>
2019-11-10 13:11:06 +0100

    catching exception during parsing

--- python-scripts/classes/controller/gui_controller.py
+++ python-scripts/classes/controller/gui_controller.py
@@ -70,7 +70,7 @@ class GuiController:
         messagebox.showinfo(gui.title, "This process may take a while. Grab a something to drink! 😊")
 
         for file in self.input_files:
-            # try:
+            try:
                 cached_file = get_cached_file_for(path_leaf(file))
                 file_is_parsed_already = cached_file.is_file()
 
@@ -88,14 +88,14 @@ class GuiController:
                     create_json_from_data_item(data_items_in_file, path_leaf(cached_file).replace('.json', ''))
                 print(path_leaf(file) + " was successfully parsed.")
 
-            # except Exception as e:    # TODO catch in production
-            #     messagebox.showerror(gui.title, "Couldn't parse the timetable '"+path_leaf(file)+"' because:\n"+str(e))
+            except Exception as e:  # TODO catch in production
+                messagebox.showerror(gui.title,
+                                     "Couldn't parse the timetable '" + path_leaf(file) + "' because:\n" + str(e))
 
         messagebox.showinfo(gui.title, "Parsing finished! 😊")
 
 
 def get_data_items_from_file(file):
-
     """
     Summary
     -------
